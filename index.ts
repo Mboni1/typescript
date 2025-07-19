ebb53b8bd11ff50a42cd8c326f63b2b2c75f9ccd
stana <YOUR_EMAIL>
2025-07-19 11:37:09 +0200

    tuples

--- index.ts
+++ index.ts
@@ -1,15 +1,2 @@
-const items = [
-  { price: 2 },
-  { quantity: 4 },
-  { discount: 0.1 }
-];
-
-export function calculateTotal(
-    price:number,
-    quantity:number,
-    discount:number
-): number {
-    const result= price * quantity *(1-discount);
-    console.log("calculatedTotal:",result);
-    return result;
-}
+let user: [number, string] = [1, "claude"]
+user.push(1)
