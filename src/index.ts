e79d64f9f9597d281b18291cb7e61b6772a70eec
stana <YOUR_EMAIL>
2025-07-19 20:30:51 +0200

    functions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-let user: [number, string] = [1, "claude"]
-user.push(1)
+function calculateTax(income: number, taxYear= 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+        return income * 1.3;
+}
+calculateTax(10_000);
