e4117d57e55e750ea06cd3ef5348f828c2389c66
stana <YOUR_EMAIL>
2025-07-26 12:07:35 +0200

    instanceof

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-type StringOrNumber = string | number;
-
-function add1(value: StringOrNumber): StringOrNumber {
-    if (typeof value === 'string'){
-       return value + "1"  
+class Dog {
+    firstName: string;
+    lastName : string;
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName
+    }
+}
+class Cat {
+    firstName: string;
+    constructor(firstName: string){
+        this.firstName = firstName;
+    }
+}
+function getName(animal: Cat | Dog){
+    if (animal instanceof Cat){
+        console.log("the name is ", animal.firstName)
     } else {
-      return value+ 1  
-    }   
+        console.log("the name is", animal.firstName+ " "+ animal.lastName)
+    }
 }
