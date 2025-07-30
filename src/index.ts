5fc1397c219cf1c0c878c36afcd172978900e1a1
stana <YOUR_EMAIL>
2025-07-30 10:32:22 +0200

    Game of Cards

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import Deck from "./deck";
-import { Icard } from "./types";
-import {getBet, getDecision, getHandValue, getStrHand } from "/.utils";
+import { ICard } from "./type";
+import { getBet, getDecision, getHandValue, getStrHand } from "./utils";
 
-function playerTurn(playerHand: Icard[],deck: Deck): number{
+function playerTurn(playerHand: ICard[], deck: Deck): number {
     let handValue = getHandValue(playerHand);
 
     while (true) {
         const action = getDecision();
         if (action !== "hit") return handValue;
 
-        playerHand.push(deck.deal(1) [0]);
+        const newCard = deck.deal(1)[0];
+        playerHand.push(newCard);
         handValue = getHandValue(playerHand);
         console.log(`Your hand: ${getStrHand(playerHand)} (Total: ${handValue})`);
 
@@ -18,34 +19,33 @@ function playerTurn(playerHand: Icard[],deck: Deck): number{
         }
     }
 }
-function dealerTurn(dealerHand: Icard[], deck: Deck): number {
+
+function dealerTurn(dealerHand: ICard[], deck: Deck): number {
     let handValue = getHandValue(dealerHand);
 
-    while (true) {
-        console.log(
-            `Dealer's hand: ${getStrHand(dealerHand)} (Total: ${handValue})`
-        );
-        if (handValue >= 17) return handValue;
+    while (handValue < 17) {
+        const newCard = deck.deal(1)[0];
+        dealerHand.push(newCard);
         handValue = getHandValue(dealerHand);
     }
 }
 let dealerHand: Icard[] = [];
 let playerHand: Icard[] = [];
 const deck: Deck = new Deck();
-let  balance = 100
+let  balance = 100;
 
 while (balance > 0){
     console.log(`\nPlayer funds $${balance}`);
     const bet = getBet(balance);
     balance -= bet;
 
-    // Deal the cards
-    deck.reset();
-    playerHand = deck.deal(2);
-    dealerHand = deck.deal(2);
+        // Deal the cards
+        deck.reset();
+        playerHand = deck.deal(2);
+        dealerHand = deck.deal(2);
 
-    const playerValue = getHandValue(playerHand);
-    const dealerValue = getHandValue(dealerHand);
+        const playerValue = getHandValue(playerHand);
+        const dealerValue = getHandValue(dealerHand);
 
     console.log(`Your hand: ${getStrHand(playerHand)} (Total: ${playerValue})`);
     console.log(`Dealer's hand: ${getStrHand(dealerHand, true)}`);
@@ -75,7 +75,3 @@ while (balance > 0){
     }
 }
 console.log(" You ran out of money!");
-
-// Rename during export
-export { name as utilsName, log as logMessage };
-
